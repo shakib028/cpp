2ff23b6f1aca716d886f485ae31d0132c2491d67
md shakib islam <shakibho028@gmail.com>
2025-02-22 20:29:41 +0600

    inside-function.cpp

---
+++ inside-function.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class state
+{
+public:
+    void cse()
+    {
+        cout<<"welcome to cse"<<endl;
+    }
+};
+int main ()
+{
+    state obj;
+    obj.cse();
+
+    return 0;
+}
