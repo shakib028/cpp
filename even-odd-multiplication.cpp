28395865c7b128a719ae84b1ae656650b8ae6d03
md shakib islam <shakibho028@gmail.com>
2025-02-22 20:25:46 +0600

    even-odd-multiplication.cpp

---
+++ even-odd-multiplication.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class even
+{
+public:
+    void check()
+    {
+        int x;
+        int even = 1;
+        int odd =1;
+        cin>>x;
+        for(int i=1; i<=x;i++)
+        {
+            if(i%2==0)
+            {
+                even=even*i;
+            }
+            else
+            {
+               odd=odd*i;
+            }
+        }
+        cout<<"Total even: "<<even<<" "<<endl;
+        cout<<"Total odd: "<<odd<<" ";
+
+    }
+};
+int main()
+{
+    even obj;
+    obj.check();
+
+    return 0;
+}
